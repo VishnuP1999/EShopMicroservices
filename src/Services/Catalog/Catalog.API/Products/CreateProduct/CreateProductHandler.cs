62fda726c1331b77d4f3c8ecef55cfa36fb439e4
Vishnu Pillai <pillai.vishnu1999@gmail.com>
2025-07-27 17:56:37 +0530

    Second commit with logics of CQRS Interface, and CreateProductLogic

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,37 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Categotry, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+
+            // Business logic to create a product would go here.
+            // Here you would typically interact with a database or a service to create the product.    
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Categotry,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //save the database
+            //return result
+
+
+            return new CreateProductResult(Guid.NewGuid()); // Simulating product creation with a new Guid for demonstration purposes.
+        }
+       
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// ADD services to the container.
+
+
+
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+
+
+
+
+
+app.Run();
